406b5b0225e01a24ddd5094a31706abdbf18c508
Nate Bohn <nathan.bohn@cloudreach.com>
2021-09-28 16:54:22 -0500

    going with var name token

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import os
 
 def main():
     env_name = os.environ['INPUT_ENV_NAME']
-    export_var_name = os.environ['INPUT_EXPORT_VAR_NAME']
     data = os.environ[env_name]
     set_output(data)
 
 
 def set_output(data):
-    print(f"::set-output name=encoded_string::{data}", flush=True)
+    print(f"::set-output name=encoded_token::{data}", flush=True)
 
 
 if __name__ == "__main__":
