9fc39ac6d8897c9ab7b817eceee345ead38ed886
Nate Bohn <nathan.bohn@cloudreach.com>
2021-09-28 16:45:51 -0500

    adding output

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ def main():
     env_name = os.environ['INPUT_ENV_NAME']
     export_var_name = os.environ['INPUT_EXPORT_VAR_NAME']
     data = os.environ[env_name]
-    os.environ[export_var_name] = data
+    set_output(data)
 
 
-def set_output(cmd):
-    print(f"::set-output name=random_string::{cmd}", flush=True)
+def set_output(data):
+    print(f"::set-output name=encoded_string::{data}", flush=True)
 
 
 if __name__ == "__main__":
