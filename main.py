48ca7167d8001c8fad6e595dca5c5482bcb2b34b
Nate Bohn <nathan.bohn@cloudreach.com>
2021-09-28 17:15:25 -0500

    setting it before

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ def main():
 
 
 def set_output(data):
-    print(f"::add-mask:: RANDOM_KEY", flush=True)
-    print(f'echo "RANDOM_KEY={data}" >> $GITHUB_ENV')
+    print(f"::add-mask::{data}", flush=True)
     print(f"::set-output name=encoded_string::{data}", flush=True)
 
 
