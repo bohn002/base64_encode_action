cf88bbae050e5b07f781d5b60b884b735d5a6f0a
Nate Bohn <nathan.bohn@cloudreach.com>
2021-09-30 08:52:12 -0500

    b to str

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from base64 import b64encode
 def main():
     env_name = os.environ['INPUT_ENV_NAME']
     data = os.environ[env_name]
-    set_output(str(b64encode(data.encode())))
+    set_output(b64encode(data.encode()))
 
 
 def set_output(data):
+    data = data.decode()
     print(f"::add-mask::{data}", flush=True)
     print(f"::set-output name=encoded_string::{data}", flush=True)
 
